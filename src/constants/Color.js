f1478e7f640e745512382ca809ce2387e572b0b3
Andre Gomes <gomesacbc@gmail.com>
2018-08-06 12:48:21 -0500

    Add react router dom

---
+++ src/constants/Color.js
@@ -0,0 +1,8 @@
+export default {
+  lightGreen: 'rgb(140,178,71)',
+  darkGreen: 'rgb(75,97,44)',
+  lightPurple: 'rgb(122,49,146)',
+  darkPurple: 'rgb(58,20,76)',
+  lightGrey: 'rgb(250,248,250)',
+  lightBrown: 'rgb(243, 237, 231)',
+};
